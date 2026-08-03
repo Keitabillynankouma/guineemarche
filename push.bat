17027d4702c78953dae8e652c253faaeb9268fa9
billy2228 <billynankoumak2@gmail.com>
2026-08-03 17:17:21 +0300

    fix: web SellerEarningsPage - 6 operateurs payout + setUser store + fix payout_phone path + fix En especes

---
+++ push.bat
@@ -0,0 +1,8 @@
+@echo off
+cd /d C:\Users\DNTCP\guineemarche
+git add -A
+git commit -m "fix: web SellerEarningsPage - 6 operateurs payout + setUser store + fix payout_phone path + fix En especes"
+git push
+echo.
+echo === PUSH TERMINE ===
+pause
